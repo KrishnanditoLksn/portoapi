34cee52c4971082f989d1befb6e117111b8db663
KrishnanditoLksn <krislaksana123@gmail.com>
2025-08-14 19:24:55 +0700

    fix:module integration in user repository

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,11 +1,15 @@
+// user.module.ts
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './entity/user.entity';
+import { UserService } from './user.service';
 import { UserController } from './user.controller';
 
 @Module({
-    imports:[],
-    providers:[],
-    controllers:[UserController]
+    imports: [
+        TypeOrmModule.forFeature([User])
+    ],
+    controllers:[UserController],
+    providers: [UserService]
 })
-export class UserModule {
-    
-}
+export class UserModule { }
