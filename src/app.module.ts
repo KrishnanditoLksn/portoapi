c49bf35bea3545e8831de88074032f927677d86e
KrishnanditoLksn <krislaksana123@gmail.com>
2025-08-24 22:31:32 +0700

    feat: configure TypeORM to use environment variables for database connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,18 +4,27 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './user/entity/user.entity';
 import { Category } from './category/entity/category';
 import { CategoryModule } from './category/category.module';
+import { env } from 'process';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: '',
-      database: 'myportoapi_db',
-      entities: [User, Category],
-      synchronize: true,
+    ConfigModule.forRoot({
+      envFilePath: ".env"
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.get<string>("DATABASE_HOST"),
+        port: configService.get<number>("DATABASE_PORT"),
+        username: configService.get<string>("DATABASE_USERNAME"),
+        password: configService.get<string>("DATABASE_PASSWORD"),
+        database: configService.get<string>("DATABASE_NAME"),
+        entities: [User, Category],
+        synchronize: true,
+      })
     }),
     UserModule,
     CategoryModule
